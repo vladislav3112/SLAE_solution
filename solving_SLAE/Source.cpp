f7df55447ccb96e01c146d7cdfd1bfbe64afa7d3
dyushakolesov <dyushakolesov@mail.ru>
2020-04-11 15:08:57 +0300

    fixed transporation vector values

--- solving_SLAE/Source.cpp
+++ solving_SLAE/Source.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
-
+#include<time.h>
 using namespace std;
 
 void LU(vector <vector <double>> A, vector <vector <double>> &L,
-	vector <vector <double>> &U, int n,vector<int>transp) // transp - vector перестановок
+	vector <vector <double>> &U, int n,vector<int> &transp) // transp - vector перестановок
 {
 	U = A;
 	vector<double> tmp(n);
@@ -15,7 +15,7 @@ void LU(vector <vector <double>> A, vector <vector <double>> &L,
 			else {
 				transp.push_back(i);
 				tmp = U[i];
-				for (int idx = i; idx++; idx < n-1)  //перестановка строки в конец
+				for (int idx = i; idx < n - 1; idx++)  //перестановка строки в конец
 				{
 					U[idx] = U[idx + 1];
 				}
@@ -26,16 +26,7 @@ void LU(vector <vector <double>> A, vector <vector <double>> &L,
 	{
 		for (int i = k - 1; i < n; i++)
 			for (int j = i; j < n; j++) {
-				if(U[i][i]!=0)L[j][i] = U[j][i] / U[i][i];//Если U[i][i]=0,то U[j]- в конец.
-				else {
-					transp.push_back(i);
-					tmp = U[n - 1];
-					for (int idx = i; idx++; idx < n - 1)  //перестановка строки в конец
-					{
-						U[idx] = U[idx + 1];
-					}
-					U[n - 1] = tmp;
-				}
+				L[j][i] = U[j][i] / U[i][i];
 			}
 		for (int i = k; i < n; i++)
 			for (int j = k - 1; j < n; j++)
@@ -68,6 +59,19 @@ void slae_solution(vector <vector <double>> L,
 	vector <vector <double>> U, vector<double>b, vector<double>&x, vector<int> transp, int n) {
 	double sum = 0;
 	vector <double> y(n);
+	//согласование столбца b с матрицами L и U:
+	double tmp;
+	if (!transp.empty()) {
+		for (int j = 0; j < transp.size(); j++) {
+			tmp = b[transp[j]];
+			for (int idx = transp[j]; idx < n - 1; idx++)  //перестановка строки в конец
+			{
+				b[idx] = b[idx + 1];
+			}
+			b[n - 1] = tmp;
+		}
+	}
+
 	//1 step  Ly=b,  where Ux=y :
 	y[0] = b[0];
 
@@ -91,41 +95,44 @@ void slae_solution(vector <vector <double>> L,
 		}
 		x[i] = (x[i] + y[i]) / U[i][i];
 	}
-	double tmp;
-	int i;
-	//перестановка решений в зависимости от вектора перестановок:
-	while (!transp.empty()) {
-		i = transp.back();
-		transp.pop_back();
-		tmp = x[n - 1];
-		for (int idx = i+1; idx++; idx < n - 1)  //перестановка строки в конец
-		{
-			x[idx] = x[idx-1];
-		}
-		x[i] = tmp;
-	}
 }
 
 void reverse_matrix(vector <vector <double>> L,
-	vector <vector <double>> U, vector<double>b, vector < vector <double>>&A, int n) {
+	vector <vector <double>> U, vector<double>b, vector < vector <double>>&A, vector<int> transp, int n) {
 	
 	vector<double> e(n);
 	vector<double> x(n);
+	vector<int> transportation;
 	for (int i = 0; i < n; i++)
 	{
 		e[i] = 1;
-		slae_solution(L, U, e, x, n);
+		slae_solution(L, U, e, x, transportation, n);
 		A[i] = x;
 		e[i] = 0;
 	}
-	double tmp = 0;
+	vector<double> tmp(n);
+	int i;
+	
+	//перестановка в зависимости от вектора перестановок:
+ 	while (!transp.empty()) {
+		i = transp.back();
+		transp.pop_back();
+		tmp = A[n - 1];
+		for (int idx = n - 1; idx > i; idx--)  //перестановка строки в конец
+		{
+			A[idx] = A[idx - 1];
+		}
+		A[i] = tmp;
+	}
+
+	double temp_num;
 	for (int i = 1; i < n; i++)
 	{
 		for (int j = 0; j <i; j++)
 		{
-			tmp = A[i][j];
+			temp_num = A[i][j];
 			A[i][j] = A[j][i];
-			A[j][i] = tmp;
+			A[j][i] = temp_num;
 		}
 	}
 
@@ -156,6 +163,7 @@ int main()
 	vector <vector <double>> A(n), L(n), U(n), R(n), L_(n), U_(n),A_1(n), TMP(n);
 	vector <double> b(n);
 	vector <double> y(n),x(n);
+	srand(time(NULL));
 	for (int i = 0; i < n; i++)
 	{
 		b[i] = rand() % 20;
@@ -167,12 +175,11 @@ int main()
 			R[i].push_back(0);
 		}
 	}
-	vector<double> transporation(n);
-	LU(A, L, U, n,transporation);
-	for (int i = 0; i < n; i++)det *= U[i][i];
-
+	vector<int> transporation;
 	cout << "Fisrt matrix" << endl;
 	show(A, n);
+	LU(A, L, U, n, transporation);
+	for (int i = 0; i < n; i++)det *= U[i][i];
 	cout << "U matrix" << endl;
 	show(U, n);
 	cout << "L matrix" << endl;
@@ -182,11 +189,14 @@ int main()
 	show(R, n);
 	cout << "detA= " <<det<<endl;
 	cout << "b=  "<<endl;
+	if (!transporation.empty())
+		cout << "swap";
 	for (int i = 0; i < n; i++) cout << b[i] << " " << endl;
 	slae_solution(L, U, b, x, transporation, n);
 	cout << "x=  " << endl;
 	for (int i = 0; i < n; i++) cout << x[i] << " " << endl;
-	reverse_matrix(L, U, b, A_1, n);
+
+	reverse_matrix(L, U, b, A_1, transporation, n);
 	
 	cout << "A^-1=  " << endl;
 	show(A_1, n);
